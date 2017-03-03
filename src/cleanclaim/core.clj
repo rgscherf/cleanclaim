c0716fe3b0b5616598232366c2ae2b37631f1a89
rgscherf <rgscherf@gmail.com>
2017-03-03 10:27:54 -0500

    sheet read

--- src/cleanclaim/core.clj
+++ src/cleanclaim/core.clj
@@ -1,6 +1,16 @@
-(ns cleanclaim.core)
+(ns cleanclaim.core
+  (:require [dk.ative.docjure.spreadsheet :as sheet]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(comment
+  (def sh (sheet/load-workbook "testbook.xlsx"))
+
+  (defn rows-of
+    [sheetname]
+    (->> sh
+         (sheet/select-sheet sheetname)
+         sheet/row-seq
+         (map #(->> %
+                    sheet/cell-seq
+                    (map sheet/read-cell)))))
+
+  (rows-of "testsheet-one"))
