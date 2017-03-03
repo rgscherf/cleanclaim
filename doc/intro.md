b2891d2d6ca6904aa47755493c4fb673875eed32
rgscherf <rgscherf@gmail.com>
2017-03-03 09:07:27 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cleanclaim
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cleanclaim/core_test.clj
@@ -0,0 +1,7 @@
+(ns cleanclaim.core-test
+  (:require [clojure.test :refer :all]
+            [cleanclaim.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
